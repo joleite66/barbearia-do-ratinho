14983dbeaac76ab22ce971a00742fdc01ecb3dff
joão <joleite66@gmail.com>
2023-03-12 19:06:28 +0000

    Fixing a bug with dependencies

--- src/components/Serviços.jsx
+++ src/components/Serviços.jsx
@@ -16,7 +16,48 @@ const Serviços = () => {
           <h2 className="tex-2xl font-bold text-center py-8">
             Corte de cabelo
           </h2>
-          <p className="text-center txt-4xl font-bold"> Corte de cabelo feito por um dos nossos barbeiros</p>
+          <p className="text-center txt-4xl font-bold">
+            {" "}
+            Corte de cabelo feito por um dos nossos barbeiros
+          </p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">6€</p>
+            <p className="py-2 border-b mx-8 "></p>
+            <p className="py-2 border-b mx-8 "></p>
+          </div>
+        </div>
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={logo_cabelo}
+            alt="logo_cabelo"
+          />
+          <h2 className="tex-2xl font-bold text-center py-8">
+            Corte de cabelo
+          </h2>
+          <p className="text-center txt-4xl font-bold">
+            {" "}
+            Corte de cabelo feito por um dos nossos barbeiros
+          </p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">6€</p>
+            <p className="py-2 border-b mx-8 "></p>
+            <p className="py-2 border-b mx-8 "></p>
+          </div>
+        </div>
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={logo_cabelo_barba}
+            alt="logo_cabelo_barba"
+          />
+          <h2 className="tex-2xl font-bold text-center py-8">
+            Corte de cabelo
+          </h2>
+          <p className="text-center txt-4xl font-bold">
+            {" "}
+            Corte de cabelo feito por um dos nossos barbeiros
+          </p>
           <div className="text-center font-medium">
             <p className="py-2 border-b mx-8 mt-8">6€</p>
             <p className="py-2 border-b mx-8 "></p>
