88d27cce20b3cefe66ac73f249cc982bc10844c5
joão <joleite66@gmail.com>
2023-03-12 20:49:48 +0000

    Adding services component

--- src/components/Serviços.jsx
+++ src/components/Serviços.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import logo_barba from "../assets/logo_barba.jpg";
-import logo_cabelo from "../assets/logo_cabelo.jpg";
-import logo_cabelo_barba from "../assets/logo_cabelo_barba.jpg";
+import logo_barba from "../assets/logo_barba.png";
+import logo_cabelo from "../assets/logo_cabelo.png";
+import logo_cabelo_barba from "../assets/logo_cabelo_barba.png";
 
 const Serviços = () => {
   return (
@@ -14,11 +14,11 @@ const Serviços = () => {
             alt="logo_barba"
           />
           <h2 className="tex-2xl font-bold text-center py-8">
-            Corte de cabelo
+          BARBA
           </h2>
           <p className="text-center txt-4xl font-bold">
             {" "}
-            Corte de cabelo feito por um dos nossos barbeiros
+            Corte de Barba
           </p>
           <div className="text-center font-medium">
             <p className="py-2 border-b mx-8 mt-8">6€</p>
@@ -32,15 +32,10 @@ const Serviços = () => {
             src={logo_cabelo}
             alt="logo_cabelo"
           />
-          <h2 className="tex-2xl font-bold text-center py-8">
-            Corte de cabelo
-          </h2>
-          <p className="text-center txt-4xl font-bold">
-            {" "}
-            Corte de cabelo feito por um dos nossos barbeiros
-          </p>
+          <h2 className="tex-2xl font-bold text-center py-8">CABELO</h2>
+          <p className="text-center txt-4xl font-bold"> Corte de Cabelo</p>
           <div className="text-center font-medium">
-            <p className="py-2 border-b mx-8 mt-8">6€</p>
+            <p className="py-2 border-b mx-8 mt-8">10€</p>
             <p className="py-2 border-b mx-8 "></p>
             <p className="py-2 border-b mx-8 "></p>
           </div>
@@ -52,14 +47,14 @@ const Serviços = () => {
             alt="logo_cabelo_barba"
           />
           <h2 className="tex-2xl font-bold text-center py-8">
-            Corte de cabelo
+            CABELO & BARBA
           </h2>
           <p className="text-center txt-4xl font-bold">
             {" "}
-            Corte de cabelo feito por um dos nossos barbeiros
+            Corte de Cabelo e Barba
           </p>
           <div className="text-center font-medium">
-            <p className="py-2 border-b mx-8 mt-8">6€</p>
+            <p className="py-2 border-b mx-8 mt-8">15€</p>
             <p className="py-2 border-b mx-8 "></p>
             <p className="py-2 border-b mx-8 "></p>
           </div>
